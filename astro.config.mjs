8b43dbf195b76ac3cab8f78ff7c217afbd400d76
dreykdev <dreykdev@outlook.com>
2026-03-14 12:59:56 -0700

    link shared fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import { manifest } from './src/utils/manifest';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4322/',
+  site: 'https://drakk3-blog.netlify.app',
   image: {
     remotePatterns: [{ protocol: 'https' }],
   },
